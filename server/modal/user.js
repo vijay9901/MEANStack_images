8b62ba66ba5b22ab418ba4b4cc71b2ffbf42bd49
vijay9901 <59856063+vijay9901@users.noreply.github.com>
2020-05-28 10:11:12 +0530

    Add files via upload

--- server/modal/user.js
+++ server/modal/user.js
@@ -1,1 +1,80 @@
+const mongoose = require('mongoose')
+// const validator = require('validator')
+// const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
+// const Task = require('./task')
 
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String
+    },
+    avatar: {
+        type: Buffer
+    }
+}, {
+    timestamps: true
+})
+
+// userSchema.virtual('tasks', {
+//     ref: 'Task',
+//     localField: '_id',
+//     foreignField: 'owner'
+// })
+
+// userSchema.methods.toJSON = function () {
+//     const user = this
+//     const userObject = user.toObject()
+
+//     delete userObject.password
+//     delete userObject.tokens
+
+//     return userObject
+// }
+
+// userSchema.methods.generateAuthToken = async function () {
+//     const user = this
+//     const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
+//     user.tokens = user.tokens.concat({ token })
+//     await user.save()
+
+//     return token
+// }
+
+// userSchema.statics.findByCredentials = async (email, password) => {
+//     const user = await User.findOne({ email })
+
+//     if (!user) {
+//         throw new Error('Unable to login')
+//     }
+
+//     const isMatch = await bcrypt.compare(password, user.password)
+
+//     if (!isMatch) {
+//         throw new Error('Unable to login')
+//     }
+
+//     return user
+// }
+
+// // Hash the plain text password before saving
+// userSchema.pre('save', async function (next) {
+//     const user = this
+
+//     if (user.isModified('password')) {
+//         user.password = await bcrypt.hash(user.password, 8)
+//     }
+
+//     next()
+// })
+
+// // Delete user tasks when user is removed
+// userSchema.pre('remove', async function (next) {
+//     const user = this
+//     await Task.deleteMany({ owner: user._id })
+//     next()
+// })
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
