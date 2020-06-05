b7846348a8df239e22043900fde6e5e10f2b96fc
vijay9901 <59856063+vijay9901@users.noreply.github.com>
2020-06-05 11:20:54 +0530

    Add files via upload

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,1 +1,20 @@
-aas
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
